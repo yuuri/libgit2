fd67d7e6dec20960d8f1ad69477daddfc6f3545e
Edward Thomson <ethomson@edwardthomson.com>
2020-12-08 19:46:25 +0000

    threadstate: don't use an unnecessary macro
    Now that we've reduced the usage of GIT_THREADSTATE, remove it entirely
    in favor of git_threadstate_get().

--- src/errors.c
+++ src/errors.c
@@ -33,7 +33,7 @@ static git_error tlsdata_error = {
 
 static void set_error_from_buffer(int error_class)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 	git_error *error;
 	git_buf *buf;
 
@@ -51,7 +51,7 @@ static void set_error_from_buffer(int error_class)
 
 static void set_error(int error_class, char *string)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 	git_buf *buf;
 
 	if (!threadstate)
@@ -71,7 +71,7 @@ static void set_error(int error_class, char *string)
 
 void git_error_set_oom(void)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 
 	if (!threadstate)
 		return;
@@ -90,7 +90,7 @@ void git_error_set(int error_class, const char *fmt, ...)
 
 void git_error_vset(int error_class, const char *fmt, va_list ap)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 	int error_code = (error_class == GIT_ERROR_OS) ? errno : 0;
 	git_buf *buf;
 
@@ -135,7 +135,7 @@ void git_error_vset(int error_class, const char *fmt, va_list ap)
 
 int git_error_set_str(int error_class, const char *string)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 	git_buf *buf;
 
 	GIT_ASSERT_ARG(string);
@@ -162,7 +162,7 @@ int git_error_set_str(int error_class, const char *string)
 
 void git_error_clear(void)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 
 	if (!threadstate)
 		return;
@@ -186,7 +186,7 @@ const git_error *git_error_last(void)
 	if (!git_libgit2_init_count())
 		return &uninitialized_error;
 
-	if ((threadstate = GIT_THREADSTATE) == NULL)
+	if ((threadstate = git_threadstate_get()) == NULL)
 		return &tlsdata_error;
 
 	return threadstate->last_error;
@@ -194,7 +194,7 @@ const git_error *git_error_last(void)
 
 int git_error_state_capture(git_error_state *state, int error_code)
 {
-	git_threadstate *threadstate = GIT_THREADSTATE;
+	git_threadstate *threadstate = git_threadstate_get();
 	git_error *error;
 	git_buf *error_buf;
 
